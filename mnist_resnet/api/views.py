8ef56e95c13066f8faff092d244dce7f89700668
Syed Mohammad Abuzar <sabuzar00@gmail.com>
2024-07-16 00:18:00 +0500

    added an echo api

--- mnist_resnet/api/views.py
+++ mnist_resnet/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class PredictView(APIView):
+    def post(self, request):
+        data = request.data
+        print(data)
+        prediction = data
+        return Response({'prediction': prediction}, status=status.HTTP_200_OK)
